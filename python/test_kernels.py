5073ccf87755c003d3a42062461be4a3fdb475bb
palomonserrat <paloporco@gmail.com>
2026-07-13 14:42:55 -0300

    cambio test jernels

--- python/test_kernels.py
+++ python/test_kernels.py
@@ -20,7 +20,7 @@ def main():
     # "box_blur"
     # "laplacian"
     # "sharpening"
-    filter_name = "box_blur"
+    filter_name = "sharpening"
 
     if filter_name == "box_blur":
         kernel_float = box_blur_kernel(size=5)
